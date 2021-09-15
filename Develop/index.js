22f09c3379f9de9776c0c65a4c2d2c451c73fcfd
JD Rowley <jdrowley1989@gmail.com>
2021-09-15 14:25:08 -0600

    added function to create md file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {
+    fs.writeFile('./readme.md', process.argv[2], err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('Readme Written!');
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+};
 
 // Function call to initialize app
 init();
