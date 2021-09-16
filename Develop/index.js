5e5192ae430b7e0ae47768193926486c87880aff
JD Rowley <jdrowley1989@gmail.com>
2021-09-16 15:44:45 -0600

    created template literal for md file

--- Develop/index.js
+++ Develop/index.js
@@ -1,101 +1,101 @@
+const profileDataArgs = process.argv.slice(2);
+const [title] = profileDataArgs;
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
-    {
-        type: 'input',
-        name: 'githubName',
-        message: 'What is your GitHub username?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your e-mail address?'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Write a description of your project.'
-    },
-    {
-        type: 'confirm',
-        name: 'confirmContents',
-        message: 'Will this ReadMe.md require a "Table of Contents?"',
-        default: false
-    },
-    {
-        type: 'input',
-        name: 'contents',
-        message: 'Enter your "Table of Contents."',
-        when: ({ confirmContents }) => {
-            if (confirmContents) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmtInstallation',
-        message: 'Does your project require installation instructions?',
-        default: false
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your project?',
-        when: ({ confirmInstallation }) => {
-            if (confirmInstallation) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use your project?'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Are there any contribution guidelines?'
-    },
-    {
-        type: 'input',
-        name: 'testing',
-        message: 'How do you test this project?'
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Which license would you like to use?',
-        choices: ['Apache', 'MIT', 'GNU', 'CC0-1.0', 'Unlicense']
-    }
-])
-.then(data => {
-    const filename = 
-        data.name
-            .toLowerCase()
-            .split(' ')
-            .join('') + '.json';
-});
+// const questions = inquirer.prompt([
+//     {
+//         type: 'input',
+//         name: 'githubName',
+//         message: 'What is your GitHub username?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'email',
+//         message: 'What is your e-mail address?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'title',
+//         message: 'What is the title of your project?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'Write a description of your project.'
+//     },
+//     {
+//         type: 'confirm',
+//         name: 'confirmContents',
+//         message: 'Will this ReadMe.md require a "Table of Contents?"',
+//         default: false
+//     },
+//     {
+//         type: 'input',
+//         name: 'contents',
+//         message: 'Enter your "Table of Contents."',
+//         when: ({ confirmContents }) => {
+//             if (confirmContents) {
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     },
+//     {
+//         type: 'confirm',
+//         name: 'confirmtInstallation',
+//         message: 'Does your project require installation instructions?',
+//         default: false
+//     },
+//     {
+//         type: 'input',
+//         name: 'installation',
+//         message: 'How do you install your project?',
+//         when: ({ confirmInstallation }) => {
+//             if (confirmInstallation) {
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     },
+//     {
+//         type: 'input',
+//         name: 'usage',
+//         message: 'How do you use your project?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'contribution',
+//         message: 'Are there any contribution guidelines?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'testing',
+//         message: 'How do you test this project?'
+//     },
+//     {
+//         type: 'checkbox',
+//         name: 'license',
+//         message: 'Which license would you like to use?',
+//         choices: ['Apache', 'MIT', 'GNU', 'CC0-1.0', 'Unlicense']
+//     }
+// ])
+// .then(data => {
+//     const filename = 
+//         data.name
+//             .toLowerCase()
+//             .split(' ')
+//             .join('') + '.json';
+// });
 
 // TODO: Create a function to write README file
-function writeToFile(filename, data) {
-    fs.writeFile('./generated-readme.md', questions, err => {
-        if (err) {
-            return console.log(err);
-        }
+function writeToFile() {
+    fs.writeFile('./generated-readme.md', generateMarkdown(title).toString(), err => {
+        if (err) throw err;
 
         console.log('Readme Written!');
     });
@@ -103,8 +103,8 @@ function writeToFile(filename, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    // writeToFile();
+    writeToFile();
 };
 
 // Function call to initialize app
-init(questions);
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+const profileDataArgs = process.argv.slice(2);
+const [title] = profileDataArgs;
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,10 +13,45 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(title) {
+  return `
+  # ${title}
+  License
+  ## Description
+  Description
+  ## Table of Contents
+  Contents
+  ## Installation
+  Install
+  ## Usage
+  Use
+  ## Contribution Guidelines
+  Contribute
+  ## Testing
+  Test
+  ## Questions
+  For questions, consult GitHub or send messages to E-mail.
+  `;
 }
+
+console.log(generateMarkdown(title))
+
+// return `# ${data.title}
+// ${data.license}
+// ## Description
+// ${data.description}
+// ## Table of Contents
+// ${data.contents}
+// ## Installation
+// ${data.installation}
+// ## Usage
+// ${data.usage}
+// ## Contribution Guidelines
+// ${data.contribution}
+// ## Testing
+// ${data.testing}
+// ## Questions
+// For questions, consult ${data.githubName} or send messages to ${email}.
+// `;
 
 module.exports = generateMarkdown;
